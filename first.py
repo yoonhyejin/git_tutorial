a91f329d3b74d2837d9be80b817c3b842822858f
Hyejin <0327jane@gmail.com>
2021-08-10 16:16:04 +0900

    Test git add

--- first.py
+++ first.py
@@ -1,4 +1,10 @@
 print("Hello, git!")
 print("Hi, git!")
+
+print("1st")
+
 print("This is the 1st sentence written is 1st-branch.")
-print("This is the 1st sentence written in 3rd-branch.")        
+print("This is the 1st sentence written in 3rd-branch.") 
+
+print("2nd")
+print("test git add .")
--- second.py
+++ second.py
@@ -1,3 +1,11 @@
+print("1st")
 print("Why don't you answer me, git?")
 
+print("2nd")
 print("This is the 1st sentence written in 2nd-branch.")
+
+print("3rd")
+print("4th")
+
+print("test git add -u")
+
